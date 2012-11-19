78b3194b1e94fb73c655cb13dac35010a000322f
David King <dking@xforty.com>
2012-11-19 11:06:51 -0500

    [GH-1] Update database dependency version to 1.3.2

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ description      "Executes specified actions on a database server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
-depends          "database"
+depends          "database", "1.3.2"
